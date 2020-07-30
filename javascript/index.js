25cb8c138749e6e0b292245de5f0dc775ae203e1
DimitriTour91 <65909959+DimitriTour91@users.noreply.github.com>
2020-07-31 01:28:57 +0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+// document.querySelector("").style.color = "green";
+// document.querySelector("button").color = "green";
+// document.querySelector("a").setAttribute("href", "https://www.google.gr/?hl=el");
+// document.firstElementChild.lastElementChild;
